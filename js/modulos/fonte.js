bf0553f6fcb98b31383f3593b73e3e0388e9f602
Nativa <92101458+GNativa@users.noreply.github.com>
2025-01-27 14:38:28 -0400

    Fonte de dados configurada para o campo "Banco".

--- js/modulos/fonte.js
+++ js/modulos/fonte.js
@@ -1,11 +1,20 @@
 class Fonte {
-    constructor(nome, camposExibicao) {
+    constructor(nome, camposExibicao, opcoesLista) {
         this.nome = nome;
         this.camposExibicao = camposExibicao ?? {"chave": "", "valor": ""};
+        this.opcoesLista = opcoesLista ?? [];
         this.dados = {};
     }
 
     definirDados(dados) {
+        for (const obj of dados) {
+            const chave = obj[this.camposExibicao["chave"]];
+            const valor = obj[this.camposExibicao["valor"]];
+
+            const opcao = new OpcaoLista(chave, `${chave} - ${valor}`);
+            this.opcoesLista.push(opcao);
+        }
+
         this.dados = dados;
     }
 }
